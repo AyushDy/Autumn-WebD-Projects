bdc8df8929e6c9a5559f3a46c1eb1d74dc869c7e
Ayush <rajeevdu972@gmail.com>
2024-10-19 01:14:23 +0530

    Updated the files with new changes

--- newsApp/script.js
+++ newsApp/script.js
@@ -1,10 +1,89 @@
-const url = 'https://newsapi.org/v2/everything?q=keyword&apiKey=c0ac0de734204609b73739f2914a5ef8';
 
+let keyword = ''
+let refKeyword = ''
+const searchField = document.querySelector('#search');
+const searchButton = document.querySelector('#go');
+const applyButton =  document.querySelector('#apply');
+
+
+let url = `https://newsapi.org/v2/everything?q=${keyword}&apiKey=c0ac0de734204609b73739f2914a5ef8`;
+
+
+searchField.addEventListener('input', setKeyword);
+searchButton.addEventListener('click', search);
+
+    const toggleButton = document.getElementById('toggle-filters');
+    const filters = document.querySelector('.hidden');
+
+    toggleButton.addEventListener('click', function () {
+        filters.classList.toggle('hidden');
+    });
+
+
+
+const languages = {
+    "ar": "Arabic",
+    "de": "German",
+    "en": "English",
+    "es": "Spanish",
+    "fr": "French",
+    "he": "Hebrew",
+    "it": "Italian",
+    "nl": "Dutch",
+    "no": "Norwegian",
+    "pt": "Portuguese",
+    "ru": "Russian",
+    "sv": "Swedish",
+    "ud": "Urdu",
+    "zh": "Chinese"
+  }
+
+
+  applyButton.addEventListener('click',applyFliters);
+
+  function applyFliters(){
+    const languageSelect = document.querySelector('#language');
+    const sortBy = document.querySelector('#sort');
+    const searchIn = document.querySelector('#searchIn')
+
+    let newQuery = '';
+
+    for(let option of languageSelect.options){
+        if(option.selected){
+            newQuery+= '&language='+option.value;
+        }
+    }
+
+    for(let option of sortBy.options){
+        if(option.selected){
+            newQuery+= '&sortBy='+option.value;
+            break;
+        }
+    }
+
+
+    for(let option of searchIn.options){
+        if(option.selected){
+            newQuery+= '&searchIn='+option.value;
+            break;
+        }
+    }
+    keyword = refKeyword;
+    keyword = keyword + newQuery;
+    console.log(keyword);
+  }
+
+  
+function setKeyword(){
+    keyword = searchField.value ;
+    refKeyword = keyword;
+}
 
 
 function search() {
     let dataArray;
-    fetch(url)
+    console.log(`https://newsapi.org/v2/everything?q=${keyword}&apiKey=c0ac0de734204609b73739f2914a5ef8`);
+    fetch(`https://newsapi.org/v2/everything?q=${keyword}&apiKey=c0ac0de734204609b73739f2914a5ef8`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
@@ -16,12 +95,15 @@ function search() {
         })
 }
 
-search();
-
 function renderData(dataArray){
     const mainBody= document.querySelector('.body');
+    mainBody.innerHTML='';
 
-    const newArray = dataArray.map(ele=>{
+    let newData = dataArray.filter(ele => {
+        return (ele.urlToImage!=null);
+    })
+
+    let newArray = newData.map(ele=>{
         return createCard(ele);
     })
 
